46ae74a17b99a6bb32d60c9aa1576553ece09a7e
root <yitooA>
2022-03-14 16:45:43 -0700

    puts last

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
+#include <stdio.h>
+
 /**
- *this is a progra that will use the puts function as stdout for printing a string
+ * main - prints string in puts
+ *
+ * Description: This is a longer description.
+ * The program prints Programming is like building a multilingual puzzle
+ * Return: 0
  */
 
-
-#include<stdio.h>
-
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
 }
