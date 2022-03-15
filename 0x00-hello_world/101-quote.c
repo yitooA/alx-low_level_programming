bda442956900a3488a94e7693e23f677b9f94c1f
root <yitooA>
2022-03-14 17:48:19 -0700

    intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,6 @@
 
 int main(void)
 {
-	system( "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
