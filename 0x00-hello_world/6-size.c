d00e8e81d8c28f9c0d5580efc4dafcc93b24217a
root <yitooA>
2022-03-14 17:10:07 -0700

    bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,12 +16,11 @@ int main(void)
 	long long ll;
 	float f;
 
-
-	printf("Size of a char: %lu.\n byte(s)",(unsigned long)sizeof(c));
-	printf("Size of an int: %lu.\n byte(s)",(unsigned long)sizeof(i));
-	printf("Size of a char: %lu.\n byte(s)",(unsigned long)sizeof(l));
-	printf("Size of a char: %lu.\n byte(s)",(unsigned long)sizeof(ll));
-	printf("Size of a char: %lu.\n byte(s)",(unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(l));
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(ll));
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(f));
 
 	return (0);
 }
