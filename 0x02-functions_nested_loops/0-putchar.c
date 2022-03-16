3a6b23c33a383f598d61823ed5d7f98c7cfa6f61
root <yitooA>
2022-03-16 16:28:05 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints string in puts
+ *
+ * Description: This is a longer description.
+ * The program prints Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This is the third part of learning low level programming and algorithms
+In this section I will be practicing functions and nested loops
